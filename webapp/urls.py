6fece67da165823688beaf47a46d2d84c7faf2fb
aarav <aaravpradhan12365@gmail.com>
2024-04-05 20:34:33 +0545

    working on dashboard

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='')
+    path('', views.home, name=''),
+    path('register', views.register, name='register'),
+    path('my-login', views.my_login, name='my-login'),
+    path("user-logout", views.user_logout, name="user-logout"),
+    path('dashboard', views.dashboard, name='dashboard')
 ]

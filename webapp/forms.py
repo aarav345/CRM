ee6fca82fc29f5101a200fdc98718e1f84345b65
aarav <aaravpradhan12365@gmail.com>
2024-04-08 11:45:06 +0545

    addedd create, update and view

--- webapp/forms.py
+++ webapp/forms.py
@@ -5,13 +5,14 @@ from django.contrib.auth.models import User
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
 
+from .models import Record
+
 
 class CreateUserForm(UserCreationForm):
 
     class Meta:
         
         model = User
-
         fields = ['username', 'password1', 'password2']
 
 
@@ -19,3 +20,21 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+
+
+
+
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+
+
+
+
+
+
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
 
 
 # for logout
@@ -80,6 +80,52 @@ def dashboard(request):
 
 
 
+@login_required(login_url='my-login')
+def create_record(request):
+    form = CreateRecordForm()
+
+    if request.method ==  'POST':
+        form = CreateRecordForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("dashboard")
+
+    context = {'form' : form}
+
+    return render(request, 'webapp/create-record.html', context = context)
+
+
+@login_required(login_url='my-login')
+def update_record(request, pk):
+    record = Record.objects.get(id=pk)
+
+    form = UpdateRecordForm( instance= record)
+
+    if request.method == 'POST':
+        form = UpdateRecordForm(request.POST, instance= record)
+
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard")
+        
+    context = {'form' : form}
+    return render(request, 'webapp/update-record.html', context= context)
+
+
+
+
+
+@login_required(login_url='my-login')
+def singular_record(request, pk):
+    all_records = Record.objects.get(id=pk)
+
+    context = {'record' : all_records}
+
+    return render(request, 'webapp/view-record.html', context=context)
+
+
 
 
 
@@ -88,3 +134,5 @@ def user_logout(request):
     auth.logout(request)
 
     return redirect("my-login")
+
+
